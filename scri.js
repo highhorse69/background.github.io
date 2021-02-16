d5ef673c78a54d98237dba5193e7d112196dc4e6
Rohan Sahani <rohan.sahani.m97@gmail.com>
2021-02-16 16:30:14 +0530

    project

--- scri.js
+++ scri.js
@@ -1,3 +1,9 @@
+var _ = require('lodash');
+
+var arr=[1,2,3,4,5,67,8];
+console.log('answer:', _.without(arr,67));
+
+
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
